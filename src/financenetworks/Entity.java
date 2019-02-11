98422c1f29375b8b7b5b4feef751095190206eaa
Taylor Spears <tcs@tcspears.net>
2019-02-11 10:50:52 +0000

    First full run with data to 2015

--- src/financenetworks/Entity.java
+++ src/financenetworks/Entity.java
@@ -24,6 +24,8 @@
 
 package financenetworks;
 
+import java.util.Objects;
+
 /**
  *
  * @author Taylor Spears (University of Edinburgh)
@@ -41,6 +43,33 @@ public abstract class Entity
     public String toString()
     {
         return name + ":" + getEntityType();
+    }
+    
+    @Override
+    public boolean equals(Object o)
+    {
+        if(o == this)
+        {
+            return true;
+        }
+        
+        if(!(o instanceof Entity))
+        {
+            return false;
+        }
+        
+        Entity c = (Entity) o;
+        
+        return this.name.equals(c.name);
+        
+    }
+
+    @Override
+    public int hashCode() 
+    {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        return hash;
     }
     
     public abstract String getEntityType();
